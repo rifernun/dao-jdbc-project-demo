c87a3cbe35120a59cb53407cd9e2894c582a520b
Richard Nunes <rifernun@gmail.com>
2026-02-16 16:40:02 -0300

    feat: SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,8 +13,8 @@ public class Program {
         Department obj = new Department(1, "Books");
         Seller obj1 = new Seller(1, "Joseph", "joseph@gmail.com", new Date(), 2200.0, obj);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(obj1);
-        System.out.println();
-        System.out.println(obj);
     }
 }
