7f618a2ddf3914e6754b694e5eb9bf0591cfdd0b
Richard Nunes <rifernun@gmail.com>
2026-02-16 16:22:47 -0300

    feat: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+        Seller obj1 = new Seller(1, "Joseph", "joseph@gmail.com", new Date(), 2200.0, obj);
 
+        System.out.println(obj1);
+        System.out.println();
         System.out.println(obj);
     }
 }
