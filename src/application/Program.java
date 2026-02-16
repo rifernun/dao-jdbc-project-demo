bb273dfe9c1a914f1e5c9bbb7dac3e27f0de8957
Richard Nunes <rifernun@gmail.com>
2026-02-16 17:59:59 -0300

    feat: delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,9 +8,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
@@ -36,5 +38,13 @@ public class Program {
         seller.setName("Marta Wayne");
         sellerDao.update(seller);
         System.out.println("Updated!");
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deleted!");
+
+        sc.close();
     }
 }
