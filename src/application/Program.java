11090128b1fd857881cf0d2f710c4fd232732728
Richard Nunes <rifernun@gmail.com>
2026-02-16 16:58:40 -0300

    feat: findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller obj1 = new Seller(1, "Joseph", "joseph@gmail.com", new Date(), 2200.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(obj1);
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
